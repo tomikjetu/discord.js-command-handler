63e3c5a5293f5cdb177b4d2df8ddcef8276a9f35
TGamingStudio <45291673+TGamingStudio@users.noreply.github.com>
2019-11-20 13:07:43 +0100

    Uptime command

--- example-commands/uptime.js
+++ example-commands/uptime.js
@@ -1,10 +1,9 @@
-var timeString = require("../timeString")
 module.exports = {
     name: "uptime",
     category: "Main",
     description: "Writes the uptime",
     usage: "!uptime",
     run: async function (bot, command, args, message) {
-        message.channel.send("Uptime: " + timeString(Date.now() - bot.startTime))
+        message.channel.send("Uptime: " + ((Date.now() - bot.startTime) / 1000 / 60) + "minutes")
     }
 }
